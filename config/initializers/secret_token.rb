87449034a84e53d4e101653444de6d99d7220870
June Juzhen Shi <jshi@wlwjshi.vail>
2012-10-25 11:30:08 -0500

    Init PizzariaWebApp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PizzariaWebApp::Application.config.secret_token = '4527ade06472c62210e47d5699a8bfcd6746efa72ed984a74770e9b7c7cedd8b36662ba7b25dfa2081f6f94fdea012b1ee13a4447975a67ae0de422551f35e93'
